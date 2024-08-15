4152f8e52dc7bff853089d19e269428f0beea501
OualidCh11 <118737452+OualidCh11@users.noreply.github.com>
2024-08-15 10:13:37 +0100

    add entity BookedRoom and Room

--- src/main/java/com/myhotel/g_hotel/entity/BookedRoom.java
+++ src/main/java/com/myhotel/g_hotel/entity/BookedRoom.java
@@ -1,4 +1,51 @@
 package com.myhotel.g_hotel.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private String guestFullName;
+    private String guestEmail;
+    private int nbOfAdults;
+    private int nbOfChildren;
+    private int totalNbOfGuest;
+    private String bookingConfirmationCode;
+    private Room room;
+
+    private void calculateTotalNbOfGuast(){
+        this.totalNbOfGuest = this.nbOfAdults + this.nbOfChildren;
+    }
+
+    public void setNbOfAdults(int nbOfAdults) {
+        this.nbOfAdults = nbOfAdults;
+        calculateTotalNbOfGuast();
+    }
+
+    public void setNbOfChildren(int nbOfChildren) {
+        this.nbOfChildren = nbOfChildren;
+        calculateTotalNbOfGuast();
+    }
+
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
 }
--- src/main/java/com/myhotel/g_hotel/entity/Room.java
+++ src/main/java/com/myhotel/g_hotel/entity/Room.java
@@ -1,4 +1,22 @@
 package com.myhotel.g_hotel.entity;
 
+import jakarta.persistence.Entity;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Room {
+
+    private Long id;
+    private String roomType;
+    private BigDecimal priceRoom;
+    private boolean isBooked = false;
+
+    private List<BookedRoom> bookedRoomList;
+
+    public Room() {
+        this.bookedRoomList = new ArrayList<>();
+    }
 }
