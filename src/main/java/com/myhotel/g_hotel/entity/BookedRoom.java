ae2aaec86e51d13366be2bed55c55b991c000718
OualidCh11 <118737452+OualidCh11@users.noreply.github.com>
2024-08-15 10:34:39 +0100

    BookedRoom and Room modification

--- src/main/java/com/myhotel/g_hotel/entity/BookedRoom.java
+++ src/main/java/com/myhotel/g_hotel/entity/BookedRoom.java
@@ -1,9 +1,6 @@
 package com.myhotel.g_hotel.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,14 +18,24 @@ public class BookedRoom {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long bookingId;
+    @Column(name = "checkIn")
     private LocalDate checkInDate;
+    @Column(name = "checkout")
     private LocalDate checkOutDate;
+    @Column(name = "guest_FullName")
     private String guestFullName;
+    @Column(name = "guest_Email")
     private String guestEmail;
+    @Column(name = "adult")
     private int nbOfAdults;
+    @Column(name = "children")
     private int nbOfChildren;
+    @Column(name = "totsl_guest")
     private int totalNbOfGuest;
+    @Column(name = "confirmation_code")
     private String bookingConfirmationCode;
+    @ManyToOne(fetch = FetchType.LAZY )
+    @JoinColumn(name = "room_id")
     private Room room;
 
     private void calculateTotalNbOfGuast(){
--- src/main/java/com/myhotel/g_hotel/entity/Room.java
+++ src/main/java/com/myhotel/g_hotel/entity/Room.java
@@ -1,22 +1,47 @@
 package com.myhotel.g_hotel.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import java.math.BigDecimal;
+import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Room {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String roomType;
     private BigDecimal priceRoom;
     private boolean isBooked = false;
+    @Lob
+    private Blob photo;
 
+    @OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     private List<BookedRoom> bookedRoomList;
 
     public Room() {
         this.bookedRoomList = new ArrayList<>();
+    }
+
+    public void addBooking(BookedRoom bookedRoom){
+        if(bookedRoomList == null){
+            bookedRoomList = new ArrayList<>();
+        }
+        bookedRoomList.add(bookedRoom);
+        bookedRoom.setRoom(this);
+        isBooked = true;
+        String BookingCode = RandomStringUtils.randomNumeric(10);
+        bookedRoom.setBookingConfirmationCode(BookingCode);
     }
 }
