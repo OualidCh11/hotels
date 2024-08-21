982c8fc54b3a82e6b37a74c230e23d424ccec660
OualidCh11 <118737452+OualidCh11@users.noreply.github.com>
2024-08-21 13:40:23 +0100

    add repository BRdao

--- src/main/java/com/myhotel/g_hotel/repository/BookedRoomDao.java
+++ src/main/java/com/myhotel/g_hotel/repository/BookedRoomDao.java
@@ -1,4 +1,13 @@
 package com.myhotel.g_hotel.repository;
 
-public interface BookedRoomDao {
+import com.myhotel.g_hotel.entity.BookedRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookedRoomDao extends JpaRepository<BookedRoom , Long> {
+    List<BookedRoom> findByRoomId(Long roomId);
+    BookedRoom findByBookingConfirmationCode(String confirmationCode);
 }
--- src/main/java/com/myhotel/g_hotel/service/impl/BookedRoomServiceImpl.java
+++ src/main/java/com/myhotel/g_hotel/service/impl/BookedRoomServiceImpl.java
@@ -1,22 +1,29 @@
 package com.myhotel.g_hotel.service.impl;
 
 import com.myhotel.g_hotel.entity.BookedRoom;
+import com.myhotel.g_hotel.repository.BookedRoomDao;
 import com.myhotel.g_hotel.service.inter.BookedRoomService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class BookedRoomServiceImpl implements BookedRoomService {
+
+    private final BookedRoomDao bookedRoomDao;
+
     @Override
     public List<BookedRoom> getAllBookingByRoomsId(Long roomId) {
-        return new ArrayList<>();
+
+        return bookedRoomDao.findByRoomId(roomId);
     }
 
     @Override
     public List<BookedRoom> getAllBoking() {
-        return null;
+        return bookedRoomDao.findAll();
     }
 
     @Override
@@ -26,6 +33,7 @@ public class BookedRoomServiceImpl implements BookedRoomService {
 
     @Override
     public void cancelBooking(Long bookingId) {
+        bookedRoomDao.deleteById(bookingId);
 
     }
 
